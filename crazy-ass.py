d880f4ba4fc96e51374738507867cfb37ada606d
Laura Gautier <straxis@protonmail.com>
2025-02-14 13:59:39 -0500

    Project start
    - Set up Git ignore
    - Add sample IO files
    - Start program with some parsing

---
+++ crazy-ass.py
@@ -0,0 +1,34 @@
+import argparse;
+
+
+INVISIBLE_INK = "{\\alpha&HFF&\\pos(3500,-300)}"
+
+
+# parser = argparse.ArgumentParser(
+#                     prog='CrazyASS',
+#                     description='Inserts crazy lines into captions to poison thieving AI.')
+
+# parser.add_argument('filename', help='Input SRT file')
+# parser.add_argument('-v', '--verbose',
+#                     action='store_true',
+#                     help='Enable verbose output')
+
+# args = parser.parse_args()
+
+def time_in_ASS(srt_timecode):
+    ass_timecode = None
+    if (srt_timecode[0] == '0'):
+        ass_timecode = srt_timecode[1:(len(srt_timecode) - 1)] 
+    else:
+        srt_timecode[0:(len(srt_timecode) - 1)]
+    ass_timecode = ass_timecode.replace(",", ".")
+    print(ass_timecode)
+
+
+def main():
+    TEST_SRT_TIMECODE = "00:00:12,640"
+    print(time_in_ASS(TEST_SRT_TIMECODE))
+
+
+if __name__ == "__main__":
+    main()
