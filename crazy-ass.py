72fbb18d5076de963b9a0d1c2aea071e1932d5c1
Laura Gautier <straxis@protonmail.com>
2025-02-14 16:28:28 -0500

    Parse SRT
    - Not fully working because I had a better idea to try later

--- crazy-ass.py
+++ crazy-ass.py
@@ -8,13 +8,14 @@ parser = argparse.ArgumentParser(
                     prog='CrazyASS',
                     description='Inserts crazy lines into captions to poison thieving AI.')
 
-# parser.add_argument('filename', help='Input SRT file')
+parser.add_argument('file', help='Input SRT file')
 parser.add_argument('-v', '--verbose',
                     action='store_true',
                     help='Enable verbose output')
 
 args = parser.parse_args()
 
+# Take the single bit of time in SRT and convert it to ASS.
 def time_in_ASS(srt_timecode):
     ass_timecode = None
     if (srt_timecode[0] == '0'):
@@ -24,11 +25,37 @@ def time_in_ASS(srt_timecode):
     ass_timecode = ass_timecode.replace(",", ".")
     return ass_timecode 
 
+# Take the SRT timestamp line and convert it to ASS
+def timeline_in_ASS(srt_time):
+    ass_time = "Dialogue: 0,"
+    ass_time += time_in_ASS(srt_time[0:12]) + "," + time_in_ASS(srt_time[17:])
+    ass_time += ",Default,,0,0,0,,"
+    return ass_time
 
+# Parse SRT dialogue
+def dialogue_in_ass(in_file):
+    line = in_file.readline()
+    print_newline = false;
+    while (line & line != "\n"):
+        print(line)
+        if print_newline:
+            print("\\N")
+
+# Parse SRT captions line by line,
+# converting them to ASS captions
+# while additionally inserting AI poison.
 def main():
-    TEST_SRT_TIMECODE = "10:00:12,640"
-    print(time_in_ASS(TEST_SRT_TIMECODE))
-
+    # outfile = open("output.ass", "w")
+    with open(args.file, "r") as input_file:
+        curr_line = input_file.readline()
+        while curr_line:
+            # if (curr_line.isdigit()):
+            print(len(curr_line.replace('\n', '').strip()))
+            print(len("1"))
+            # print(timeline_in_ASS(input_file.readline()))
+            break;
+                # print(dialogue_in_ASS(input_file) + "\n")
+            # curr_line = input_file.readline()
 
 if __name__ == "__main__":
     main()
